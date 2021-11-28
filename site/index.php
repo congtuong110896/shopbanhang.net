94df87d62eb2695eaf5f75c2a35e80684f9b2ee1
congtuong110896 <congtuong110896@gmail.com>
2021-11-28 17:31:40 +0700

    ver1

---
+++ site/index.php
@@ -0,0 +1,18 @@
+<?php 
+//router
+session_start();
+require '../config.php';
+require '../connectDB.php';
+
+require '../bootstrap.php';
+
+require '../vendor/autoload.php';
+$c = $_GET['c'] ?? 'home';
+$a = $_GET['a'] ?? 'index';
+$controller = ucfirst($c) . 'Controller';
+require "controller/$controller.php";
+$controller = new $controller();
+$controller->$a();
+
+
+?>
